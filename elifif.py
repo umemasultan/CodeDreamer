e793edc2d93016f4759562dccb91d5c948e2f595
umemasultan <umemasultan11@gmail.com>
2025-01-11 06:20:16 +0500

    Add files via upload
    if/elif/else conditon

---
+++ elifif.py
@@ -0,0 +1,12 @@
+
+#CheckTem=int(input("Check The Today tempreture"))
+#if CheckTem>=56:
+ #    print("its a hot today")
+#elif CheckTem>=44:
+ #   print("its a little hot today")
+#elif CheckTem>=36:
+ #   print("its a little hotter today")
+#elif CheckTem>=32:
+ #   print("it is very hot today")
+#else:
+ #   print("its too hot today so don't go any wear")
